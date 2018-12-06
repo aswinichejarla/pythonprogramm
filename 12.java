971d62b519a3684a1a1031d14ad279056040b5af
aswinichejarla <42571466+aswinichejarla@users.noreply.github.com>
2018-12-06 12:45:28 +0530

    Create 12.java

---
+++ 12.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.lang.*;
+class aswini
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        
+        int  s=sc.nextInt();
+        int sum=0;
+        while(s!=0)
+        {
+            int r=s%10;
+            sum=sum+(r*r);
+             s=s/10;
+        }
+            System.out.print(sum);
+            
+    }
+}
+        
