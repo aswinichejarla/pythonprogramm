e6fee129600cf5826c8af6e41ac8ec4a212b513c
aswinichejarla <42571466+aswinichejarla@users.noreply.github.com>
2018-12-11 11:12:11 +0530

    Create pl188.py

---
+++ pl188.py
@@ -0,0 +1,21 @@
+x,b,c=map(int,raw_input().split())
+if(x>b and x>c):
+  num1=x
+  num2=b
+  num3=c
+elif(b>c and b>x):
+  num1=b
+  num2=x
+  num3=c
+else:
+  num1=c
+  num2=x
+  num3=b
+num1=num1**2
+num2=num2**2
+num3=num3**2
+num4=num2+num3
+if(num1==num4):
+  print('yes')
+else:
+  print('no')
